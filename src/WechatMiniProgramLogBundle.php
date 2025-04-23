0aaae3760b806e8103f93508a7f9eac2c8ddfa19
github-ci <you@example.com>
2025-04-23 10:09:21 +0000

    chore: 移除无用依赖和配置

--- src/WechatMiniProgramLogBundle.php
+++ src/WechatMiniProgramLogBundle.php
@@ -3,17 +3,7 @@
 namespace WechatMiniProgramLogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '运维中心')]
-class WechatMiniProgramLogBundle extends Bundle implements BundleDependencyInterface
+class WechatMiniProgramLogBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \Tourze\Symfony\CronJob\CronJobBundle::class => ['all' => true],
-            \WechatMiniProgramBundle\WechatMiniProgramBundle::class => ['all' => true],
-        ];
-    }
 }
